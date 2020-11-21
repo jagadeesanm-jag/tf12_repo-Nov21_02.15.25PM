35d10d9f38f0a92ba12f44e0a21ed1d50fa3a394
Jagadeesan Murugesan <jmurugesan@sinmackjemd6m.paloaltonetworks.local>
2020-11-21 14:16:17 +0530

    Modified files:[main.tf]

--- dynamic-blocks-and-splat-expressions/main.tf
+++ dynamic-blocks-and-splat-expressions/main.tf
@@ -45,3 +45,5 @@ output "from_ports" {
   value = aws_security_group.vault.ingress[*].from_port
   #value = aws_security_group.vault.ingress.*.from_port
 }
+
+# Modified [main.tf] on [2020-11-21 14:16:17.163929]
